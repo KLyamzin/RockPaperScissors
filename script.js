b33425c6b1fba3809d64539ec65bf525676be2e4
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-04-27 20:51:05 -0400

    Cleaned up the code.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /*
-Adding UI to the original Rock Paper Scissors game.
+Adding UI to the original Rock Paper Scissors game. I used 2 youtube tutorials by 
+Web Dev Simplified-  https://youtu.be/1yS-JV4fWqY
+Coding Artist- https://youtu.be/Iwvf9iBP04M
 */
-console.log("Welcome To The Rock Paper Scissors Game!");
 
 const selectionButtons = document.querySelectorAll("[data-id]");
 const finalColumn = document.querySelector('[data-final-column]');
@@ -28,13 +29,12 @@ const SELECTIONS = [
 
 selectionButtons.forEach(button => {
     button.addEventListener("click", e => {
-        const selectionName = button.dataset.id
+        const selectionName = button.dataset.id;
         const selection = SELECTIONS.find(sel => sel.name === selectionName);
 
         // I really struggled to come up with this 2 lines of code. They remove the created div before adding a new div.
-        const delElm = document.querySelectorAll('.result-selection')
-        delElm.forEach(del => { del.remove() })
-
+        const delElm = document.querySelectorAll('.result-selection');
+        delElm.forEach(del => { del.remove() });
         makeSelection(selection);
     })
 })
@@ -48,105 +48,46 @@ function makeSelection(selection) {
     addSelectionResult(aiSelection, aiWinner);
     addSelectionResult(selection, yourWinner);
 
-    // increment score
+    // add incremented score to display. also show the status
     if (yourWinner) {
         incrementScore(playerScoreSpan);
-        gameStatus.innerText = "You Win!"
-        gameStatus.classList.add('victory')
-        gameStatus.classList.remove('defeat', 'tie')
+        gameStatus.innerText = "You Win!";
+        gameStatus.classList.add('victory');
+        gameStatus.classList.remove('defeat', 'tie');
     }
     else if (aiWinner) {
         incrementScore(aiScoreSpan);
-        gameStatus.innerText = "You Lost!"
-        gameStatus.classList.add('defeat')
-        gameStatus.classList.remove('victory', 'tie')
+        gameStatus.innerText = "You Lost!";
+        gameStatus.classList.add('defeat');
+        gameStatus.classList.remove('victory', 'tie');
     }
     else {
-        gameStatus.innerText = "Tie!"
-        gameStatus.classList.add('tie')
-        gameStatus.classList.remove('victory', 'defeat')
+        gameStatus.innerText = "Tie!";
+        gameStatus.classList.add('tie');
+        gameStatus.classList.remove('victory', 'defeat');
     }
-
-
 }
-
+// increment the score
 function incrementScore(scoreSpan) {
     scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
 }
-
+// display the current selections for each player
 function addSelectionResult(selection, winner) {
-    const div = document.createElement('div')
-    div.innerText = selection.emoji
-    div.classList.add('result-selection')
-    if (winner) div.classList.add('winner')
-    finalColumn.after(div)
+    const div = document.createElement('div');
+    div.innerText = selection.emoji;
+    div.classList.add('result-selection');
+    if (winner) div.classList.add('winner');
+    finalColumn.after(div);
 }
-
 
 function isWinner(playerSelection, aiSelection) {
     const result = playerSelection.beats === aiSelection.name;
-    // console.log(result);
     return result;
 }
+//generate the ai selection
 function aiGenNumb() {
     const genIndex = Math.floor(Math.random() * SELECTIONS.length);
     return SELECTIONS[genIndex];
 }
-
-
-/* //Part 2
-//Player's choice. Prompt and conversion function
-const playerSelection = function () {
-    let playerSelectionAny = prompt("Type 'Rock', 'Paper', or 'Scissors':");
-
-    // this will make every word start with the capital letter
-    let test = playerSelectionAny.charAt(0).toLocaleUpperCase() + playerSelectionAny.slice(1).toLocaleLowerCase();
-
-    // this will thest if the word matches the choices array
-    if (test === choices[0] || test === choices[1] || test === choices[2]) {
-        console.log("You chose: ", test);
-        return test;
-    } else {
-        alert("Try again...");
-        playerSelection();
-    }
-}; */
-
-//Part 3.
-//Play a single round of the game.
-
-/* let playerScore = 0;
-let computerScore = 0;
-const round = function (playerSelection, computerSelection) {
-    if (
-        (playerSelection === choices[0] && computerSelection === choices[2]) ||
-        (playerSelection === choices[1] && computerSelection === choices[0]) ||
-        (playerSelection === choices[2] && computerSelection === choices[1])
-    ) {
-        // adding before displaying the score
-        console.log(`You Win! You ${++playerScore}, Computer ${computerScore} `)
-    }
-    // lse if (rock & pap) || (pap & sc) || (sc & rock) - `you lose`
-    else if (
-        (playerSelection === choices[0] && computerSelection === choices[1]) ||
-        (playerSelection === choices[1] && computerSelection === choices[2]) ||
-        (playerSelection === choices[2] && computerSelection === choices[0])
-    ) {
-        console.log(`You loose! You ${playerScore}, Computer ${++computerScore} `)
-    } else { console.log("It's a tie!") }
-}; */
-
-/* // Part 4
-// create function > game < to call > playRound < function inside 5 times
-const game = function () {
-    for (let i = 1; i < 6; i++) {
-        // game counter
-        console.log(` \n    Game ${i} \n`)
-        // Pass the playerSelection and computerSelection as arguments invoking both at the same time
-        round(playerSelection(), computerSelection());
-    };
-};
-game();
-*/
 
 // ~~~ FIN ~~~
